f7dff8de6118b8a09ac40a22e1dceac50eace936
Facu Mazziotti <fm.mazziotti@gmail.com>
2023-11-08 11:46:06 -0300

    fix images vercel

--- src/components/Projects/ExternalLinks.jsx
+++ src/components/Projects/ExternalLinks.jsx
@@ -5,7 +5,7 @@ function ExternalLinks({ githubLink, openLink }) {
     <div className="external-links">
       <a href={githubLink} target="_blank" rel="noopener noreferrer">
         <img
-          src={"./public/github.svg"}
+          src={"/github.svg"}
           alt="github icon"
           className="GithubIcon"
         />
